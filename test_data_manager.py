7b000dc5d087aab664f7cb001e0fce09ee06c534
Francesco Battaglia <fpbattaglia@gmail.com>
2016-11-30 22:04:57 +0100

    bug fix

--- test_data_manager.py
+++ test_data_manager.py
@@ -73,7 +73,6 @@ grade: {}
     def test_create_info(self):
         from unittest.mock import patch
         import inspect
-
         cur_file = inspect.stack(0)[0][1]
         testargs = [cur_file, 1]
         with patch('sys.argv', testargs):
@@ -82,9 +81,14 @@ grade: {}
         self.assertEqual(neuroseries.track_info['entry_point'], cur_file)
 
     def test_config_files(self):
-        for grade in range(1, 5):
-            with self.subTest(grade=grade):
-                self.make_dummy_configs(grade)
-                import neuroseries
-                self.assertEqual(len(neuroseries.track_info['config']['list']), grade+2)
-                self.assertEqual(neuroseries.track_info['config']['grade'], grade)
+        from unittest.mock import patch
+        import inspect
+        cur_file = inspect.stack(0)[0][1]
+        testargs = [cur_file, 1]
+        with patch('sys.argv', testargs):
+            for grade in range(1, 5):
+                with self.subTest(grade=grade):
+                    self.make_dummy_configs(grade)
+                    import neuroseries
+                    self.assertEqual(len(neuroseries.track_info['config']['list']), grade+2)
+                    self.assertEqual(neuroseries.track_info['config']['grade'], grade)
