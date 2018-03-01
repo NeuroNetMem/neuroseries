a2cced10a7fc931d053533bb991119a0e3b129de
Francesco Battaglia <fpbattaglia@gmail.com>
2018-03-01 11:52:13 +0100

    packaging

---
+++ README.md
@@ -0,0 +1,13 @@
+### Neuroseries
+
+This package includes tools to process neural data series,
+providing function for handling of timestamped data, and for irregular 
+intervals. It is based on Pandas for most functionality, enhanced in the 
+areas where Pandas does not satisfy the needs of the neural data analyst.
+
+It also includes an experimental data manager attempting at keeping track 
+of all code used to generate each intermediate result, making use of 
+git and git-annex.
+
+This package requires Python 3 and has been tested with Python 3.5.3.
+Look at the test suite for use examples.
--- setup.py
+++ setup.py
@@ -3,18 +3,49 @@ try:
 except ImportError:
     from distutils.core import setup
 
+from codecs import open
+from os import path
+import os
+import subprocess
+
 config = {
 
 }
 
+current_path = os.getcwd()
+try:
+    os.chdir(os.path.dirname(__file__))
+    GIT_VERSION = subprocess.check_output(["git", "describe"]).strip().decode('utf-8')
+    GIT_VERSION = GIT_VERSION.split('-')[0]
+except subprocess.CalledProcessError as e:
+    GIT_VERSION = "0.1"
+os.chdir(current_path)
+
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(description='neuro time series handling utilities',
+      long_description=long_description,
+      url='https://github.com/MemDynLab/neuroseries',
       author='Francesco P. Battaglia',
       author_email='fpbattaglia@gmail.com',
       version='0.1',
-      license='MIT',
-      install_requires=['numpy', 'pandas', 'scipy', 'IPython', 'psutil'],
+      license='GPLv3',
+      install_requires=['numpy', 'pandas', 'scipy', 'IPython', 'psutil', 'git'],
       packages=['neuroseries', 'dataman'],
       package_data={'resources': ['resources/*.*']},
-      include_package_data=False,
-      name='neuroseries'
+      include_package_data=True,
+      name='neuroseries',
+      classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 3 :: Only',
+            'Topic :: Scientific/Engineering :: Bio-Informatics'
+      ]
       )
