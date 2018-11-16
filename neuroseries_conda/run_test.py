4139284bc29b854c9341469566ef74b954e13914
Francesco Battaglia <fpbattaglia@gmail.com>
2018-11-16 09:54:11 +0100

    fixing tests setup

---
+++ neuroseries_conda/run_test.py
@@ -0,0 +1,7 @@
+import unittest
+import neuroseries.tests.test_neuroseries as tests
+unittest.main(module=tests)
+
+
+# suit = unittest.TestLoader().discover("test_neuroseries")
+# unittest.TextTestRunner(verbosity=2).run(suit )
